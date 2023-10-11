a1da8ec8cb42b51935c36e7ea5205e7f619c0f36
arindranr <145304938+arindranr@users.noreply.github.com>
2023-10-11 16:10:05 +0800

    Posttest4

---
+++ Posttest4/content/home.php
@@ -0,0 +1,73 @@
+<header id="home">
+    <div class="header-content">
+        <h2>Best Trip</h2>
+        <div class="line"></div>
+        <h1>Only With BlueBird</h1>
+        <a href="rent.php" class="ctn">Rent Now</a>
+    </div>
+</header>
+
+<!-- Catalog -->
+<section class="catalog" id="catalog">
+    <div class="title">
+        <h1>BlueBird Bus</h1>
+        <div class="line">
+        </div>
+        <div class="row">
+            <div class="col">
+                <img src="images/alpha.png" width="300px">
+                <h4>Alpha</h4>
+                <p>Best for your travel buddy. A-class bus from BlueBird.</p>
+                <a href="#" class="ctn learn-more">Learn More</a>
+            </div>
+            <div class="col">
+                <img src="images/bravo.png" width="300px">
+                <h4>Bravo</h4>
+                <p>Also best for your travel buddy, with premium facilities.</p>
+                <a href="#" class="ctn learn-more">Learn More</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="catalog" id="catalog">
+    <div class="row">
+        <div class="col">
+            <img src="images/delta.png" width="300px">
+            <h4>Delta</h4>
+            <p>Family trip with less people? Choose this. </p>
+            <a href="#" class="ctn learn-more">Learn More</a>
+        </div>
+        <div class="col">
+            <img src="images/metta.png" width="300px">
+            <h4>Metta</h4>
+            <p>May not be the best for your life but best for your travel buddy.</p>
+            <a href="#" class="ctn learn-more">Learn More</a>
+        </div>
+    </div>
+</section>
+
+<!-- About Us -->
+<section class="about" id="about">
+    <div class="title">
+        <h1>About Us</h1>
+    </div>
+    <div class="line"></div>
+    <div class="content">
+        <div class="image">
+            <img src="images/foto-arin.jpg" alt="" width=" 50px">
+        </div>
+        <div>
+            <h3>Tentang Saya</h3>
+            <br />
+            <div>Nama: Arindra Nurshadrina Ramadini</div>
+            <div>NIM : 2209106070 </div>
+            <div>TTL : 8 November 2004 </div>
+            <div>E-mail : arindra.nr@gmail.com </div>
+        </div>
+    </div>
+</section>
+
+<section class="footer">
+    <p>Copyright © 2022 Blue Bird Group. All Rights Reserved</p>
+</section>
